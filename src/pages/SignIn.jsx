437ff19022795cb7a44032509da5602963e696e0
Marc Nkengfac <nkengfacmarc@gmail.com>
2025-11-22 18:00:23 -0500

    Sign

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,18 +1,46 @@
-export default function SignIn() {
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../services/firebase";
+import { useNavigate } from "react-router-dom";
+
+function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/mybox"); // go to MyBox after login
+    } catch (error) {
+      alert("Incorrect email or password.");
+    }
+  };
+
   return (
-    <div className="signin-page">
-      <h2>Sign in</h2>
-      <form className="auth-form">
-        <label>
-          Email
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" />
-        </label>
+    <div>
+      <h2>Sign In</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input 
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <button type="submit">Sign In</button>
       </form>
     </div>
   );
 }
+
+export default SignIn;
+
